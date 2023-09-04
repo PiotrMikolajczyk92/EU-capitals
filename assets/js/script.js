c4b2d11f3da746bdd9fc2d5d9037a9bc5b76064f
PiotrMikolajczyk92 <>
2023-09-04 11:12:44 +0000

    create list of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,143 @@
+// create an array with list of questions and answers
+
+const questions = [
+    {
+        question: "What is the capital of Estonia?",
+        answers: [
+            { text: "Warsaw", correct: false},
+            { text: "Riga", correct: false},
+            { text: "Tallinn", correct: true},
+            { text: "Vilnius", correct: false},
+        ]
+    },
+    {
+        question: "The capital of which EU member is the Northernmost?",
+        answers: [
+            { text: "Sweden", correct: false },
+            { text: "Finland", correct: true },
+            { text: "Denmark", correct: false },
+            { text: "Estonia", correct: false },
+        ]
+    },
+    {
+        question: "The capital of which EU member is Zagreb?",
+        answers: [
+            { text: "Bulgaria", correct: false },
+            { text: "Slovenia", correct: false },
+            { text: "Cyprus", correct: false },
+            { text: "Croatia", correct: true },
+        ]
+    },
+    {
+        question: "The capital of which EU member is the Southernmost?",
+        answers: [
+            { text: "Cyprus", correct: true },
+            { text: "Malta", correct: false },
+            { text: "Greece", correct: false },
+            { text: "Portugal", correct: false },
+        ]
+    },
+    {
+        question: "Which of the following languageus is NOT the official language in Brussels?",
+        answers: [
+            { text: "English", correct: true },
+            { text: "German", correct: false },
+            { text: "French", correct: false },
+            { text: "Dutch", correct: false },
+        ]
+    },
+    {
+        question: "Which of the following EU capitals has the largest population?",
+        answers: [
+            { text: "Berlin", correct: true },
+            { text: "Paris", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Rome", correct: false },
+        ]
+    },
+    {
+        question: "Which country's capital is NOT a coastal city?",
+        answers: [
+            { text: "Ireland", correct: false },
+            { text: "Greece", correct: false },
+            { text: "Italy", correct: true },
+            { text: "Finland", correct: false },
+        ]
+    },
+    {
+        question: "The Danube river is NOT flowing through which EU capital?",
+        answers: [
+            { text: "Budapest", correct: false },
+            { text: "Zagreb", correct: true },
+            { text: "Bratislava", correct: false },
+            { text: "Vienna", correct: false },
+        ]
+    },
+    {
+        question: "The capital of which EU member is the Westernmost?",
+        answers: [
+            { text: "Ireland", correct: false },
+            { text: "Portugal", correct: true },
+            { text: "Spain", correct: false },
+            { text: "none of the above", correct: false },
+        ]
+    },
+    {
+        question: "Which of the following EU capitals has the smallest population?",
+        answers: [
+            { text: "Nikosia", correct: false },
+            { text: "Luxembourg", correct: false },
+            { text: "Valletta", correct: true },
+            { text: "Ljubljana", correct: false },
+        ]
+    },
+    {
+        question: "The capital of which EU member is the Easternmost?",
+        answers: [
+            { text: "Finland", correct: false },
+            { text: "Estonia", correct: false },
+            { text: "Romania", correct: false },
+            { text: "Cyprus", correct: true },
+        ]  
+    },
+    {
+        question: "Bratislava is the capital of which of the following EU member?",
+        answers: [
+            { text: "Bulgaria", correct: false },
+            { text: "Slovenia", correct: false },
+            { text: "Slovakia", correct: true },
+            { text: "Hungary", correct: false },
+        ]
+    },
+    {
+        question: "Which of the following EU capitals is a coastal city?",
+        answers: [
+            { text: "Riga", correct: true },
+            { text: "Rome", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Sofia", correct: false },
+        ]
+    },
+    {
+        question: "Which of the nordic countries is NOT a member of the European Union?",
+        answers: [
+            { text: "Norway", correct: true },
+            { text: "Sweden", correct: false },
+            { text: "Denmark", correct: false },
+            { text: "Finland", correct: false },
+        ]
+    },
+    {
+        question: "And finally - how many members does the European Union have?",
+        answers: [
+            { text: "25", correct: false },
+            { text: "26", correct: false },
+            { text: "27", correct: true },
+            { text: "28", correct: false },
+        ]
+    }
+];
+
 function startQuiz () {
 
 }
